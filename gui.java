c37cca6f7a4f97b5995a2c652995166639a74210
Matthew Parker <chaosdemon12345@gmail.com>
2021-02-06 03:19:30 -0500

    Update gui.java
    Gui update

--- gui.java
+++ gui.java
@@ -12,18 +12,19 @@ public class gui extends JFrame{
         JButton start = new JButton("Start");
         JButton help = new JButton("Help");
         JLabel title = new JLabel("Dungeon Dwellers");
-        title.setBounds(400,100,400,200);
         start.setBounds(500,275,200,100);
         help.setBounds(500,400,200,100);
+        title.setBounds(375,50,450,300);
+        title.setFont(new Font("Verdana", Font.PLAIN, 47));
 
+        panel.setLayout(null);
         panel.add(start);
         panel.add(help);
         panel.add(title);
-        panel.setLayout(null);
+        
 
-
-        frame.add(panel);
         frame.setLayout(null);
+        frame.add(panel);
         frame.setBackground(Color.gray);
         frame.setSize(1200,750);
         frame.setVisible(true);
