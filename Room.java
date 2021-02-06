60af11986a0d7184e228f6978cb451bcac3c9a6a
Bilal Chaudhry <Bilalbc@gmail.com>
2021-02-05 21:44:00 -0500

    added cardinal directions property to each room

--- Room.java
+++ Room.java
@@ -1,2 +1,54 @@
-Room.java
+/**
+ * Room
+ */
+public class Room {
+
+    private int travelTable[];
+    private int N, E, S, W;
+    private String description;
+    private String type;
+
+
+    public Room(String type, int row, int collumn, int size){
+        if(row == 0){
+            N = -1;
+            S = 1;
+        }
+        else if(row == 1){
+            N = 1;
+            S = 1;
+        }
+        else if(row == 2){
+            N = 1;
+            S = -1;
+        }
+        if(collumn == 0){
+            E = 1;
+            W = -1;
+        }
+        else if(collumn == 1){
+            E = 1;
+            W = 1;
+        }
+        else if(collumn == 2){
+            E = -1;
+            W = 1;
+        }
+
+        this.travelTable = new int [] {N, E, S, W};
+
+        switch (type){
+            case "Combat":
+                break;
+            case "Treasure":
+                break;
+            case "Ascend":
+                break;
+        }
+
+        
+
+    }
+
+}
 
