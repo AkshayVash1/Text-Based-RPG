b7ff656bae616b4d5971e77076f87a0f7b57e0ea
Matthew Parker <chaosdemon12345@gmail.com>
2021-02-06 15:33:37 -0500

    Gui Update
    Textfields

--- gui.java
+++ gui.java
@@ -12,33 +12,33 @@ public class gui extends JFrame implements ActionListener{
 
     private JButton start,help;
     private JLabel titleCard,back;
-    private JTextField input,output;
+    private JTextArea input,output;
+    ImageIcon startBack = new ImageIcon("Icons/start_menu.jpg");
+
 
     public gui(){
         JFrame frame = new JFrame("Dungeon Dwellers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
-        ImageIcon startBack = new ImageIcon("Icons/start_menu.jpg");
         
         panel.setBounds(0,0,1200,750);
-        panel.setBackground(Color.gray);
+        panel.setBackground(Color.white);
 
-        input = new JTextField();
-        output = new JTextField();
-        input.setBounds(0,450,600,300);
-        output.setBounds(600,450,600,300);
+        input = new JTextArea();
+        output = new JTextArea();
+        output.setBounds(0,400,1200,245);
+        input.setBounds(0,655,1200,95);
         input.setBackground(Color.black);
         input.setForeground(Color.white);
         output.setBackground(Color.black);
         output.setForeground(Color.white);
         output.setEditable(false);
-        input.setHorizontalAlignment(JTextField.LEFT);
-        
         
         start = new JButton("Start");
         help = new JButton("Help");
         titleCard = new JLabel("Dungeon Dwellers");
         back = new JLabel(startBack);
+
         
         start.setBounds(350,500,200,100);
         help.setBounds(650,500,200,100);
@@ -74,13 +74,15 @@ public class gui extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand() == Actions.HELP.name()){    
             JFrame f = new JFrame();
-            JOptionPane.showMessageDialog(f,"Help");
+            JOptionPane.showMessageDialog(null,"Dungeon Dwellers is a text based dungeon crawling game.\nYou are trapped in the bottom of a dark dungeon, with no exit in sight...\n"+
+            "How far will you make it...\n\nDesigned By: Akshay Vashisht, Bilal Chaudhry, Kousha Motazedian, Matthew Parker ","Help",JOptionPane.INFORMATION_MESSAGE);
         }
         else if(e.getActionCommand() == Actions.START.name()){    
             start.setVisible(false);
             help.setVisible(false);
             input.setVisible(true);
             output.setVisible(true);
+            back.setIcon(null);
         }
 
 
