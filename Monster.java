8f3c26149659169266b7cdbec5f1c4ae33beba84
Kousha Motazedian <k.d.Motazedian@Gmail.com>
2021-02-05 22:03:06 -0500

    Monster Class with empty Monster call

--- Monster.java
+++ Monster.java
@@ -7,10 +7,14 @@ public class Monster{
     private int level;
     private int hp;
     private int armour = 10;
-    private int toHit;
+    private int hitMod;
+    Random rand = new Random();
+    private player player;
     
-    private int d(int size){return Random().nextint(size) + 1;}
+    private int d(int size){return rand.nextInt(size) + 1;}
 
+
+    public Monster(){}
 
     public Monster(String name, int level){
         this.name = name;
@@ -18,25 +22,25 @@ public class Monster{
 
         switch (name){
             case "Goblin":
-                this.hp = Random().nextInt(5) + 15;
+                this.hp = rand.nextInt(5) + 15;
                 this.xp = 100;
                 this.attack = d(8);
-                this.toHit = 2;
+                this.hitMod = 2;
                 for(int i = 1; i < level; ++i){
-                    this.hp = this.hp + Random().nextInt(5)+1;
+                    this.hp = this.hp + rand.nextInt(5)+1;
                     this.xp = this.xp + 50;
                     this.armour = this.armour + 1;
                     this.attack = this.attack + 2;
                 }
-            break;
+                break;
             
             case "Orc":
-                this.hp = Random().nextInt(5) + 20;
+                this.hp = rand.nextInt(5) + 20;
                 this.xp = 200;
                 this.attack = d(10);
-                this.toHit = 3;
+                this.hitMod = 3;
                 for(int i = 1; i < level; ++i){
-                    this.hp = this.hp +Random().nextInt(5)+1;
+                    this.hp = this.hp + rand.nextInt(5)+1;
                     this.xp = this.xp +50;
                     this.armour = this.armour +1;
                     this.attack = this.attack +2;
@@ -44,16 +48,17 @@ public class Monster{
                 break;
             
             case "Ogre":
-                this.hp = Random().nextInt(5) + 25;
+                this.hp = rand.nextInt(5) + 25;
                 this.xp = 300;
-                this.toHit = 3;
+                this.hitMod = 3;
                 this.attack = d(12);
                 for(int i = 1; i < level; ++i){
-                    this.hp = this.hp + Random().nextInt(5)+1;
+                    this.hp = this.hp + rand.nextInt(5)+1;
                     this.xp = this.xp +50;
                     this.armour = this.armour + 1;
                     this.attack = this.attack +2;
-                }                   
+                }   
+                break;            
         }
     }
 
@@ -69,7 +74,11 @@ public class Monster{
 
     public void setHP(int hp) {this.hp = hp;}
 
-    
+    public void attack(){
+        if(d(20)+this.hitMod > player.getArmor()){
+            
+        }
+    }
 
 
 
